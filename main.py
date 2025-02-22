9f50e8558fc7c0acbae96f313e5f8ac9f66158b6
avtandilkostava <akostava8@gmail.com>
2025-02-22 18:13:33 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,35 @@
 import random
 
 f = open('questions.txt', 'r')
+f1 = open('Users.txt', 'a')
 
-d = {}
 name = input('Your Name -> ')
-cnt = int(input('Number of questions'))
+cnt = int(input('Number of questions -> '))
 
+users = ['name']
+e = ''
 c = 0
-s = list(list(f.readlines().split(';')))
+s = []
+for j in range(0, int(f.readline())):
+    e = ''
+    a = f.readline().split(';')
+    for k in range(0, len(a[5]) - 1):
+        e += a[5][k]
+    a[5] = e
+    s.append(a)
+
+
 for i in range(0, cnt):
     print(''
-          '*******************************************************')
-    s1 = random.choise(s)
+          '*******************************************************'
+          '')
+    s1 = random.choice(s)
+
     print(f'{i+1}) {s1[0]}')
     print(f'A) {s1[1]}, B) {s1[2]}, C) {s1[3]}, D) {s1[4]}')
+
     ans = input('Your answer -> ')
+    print()
     t = True
     for j in range(0, 4):
         if s1[ord(ans) - 64] == s1[5]:
@@ -24,5 +39,11 @@ for i in range(0, cnt):
     else:
         print('Correct')
         c += 1
-        
+
+print(f'Result -> {c}/{cnt}')
+
+f1.write(f'{name}, {c}/{cnt} ')
+f1.write(""
+         "")
+f1.close()
 f.close()
